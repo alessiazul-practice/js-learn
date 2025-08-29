0d3c3c132ebb2af4441ffea49a5841f649aa986e
alessiazul <alessiazul@gmail.com>
2025-08-28 22:00:31 -0500

    manual multiplication

---
+++ 01-basics/002-mult-manual.js
@@ -0,0 +1,14 @@
+// 1 x 1 = 1
+// 1 x 2 = 2
+// 1 x 3 = 3
+console.log("Tabla del 1")
+console.log("1 * 1 = 1");
+console.log("1 * 2 = 2");
+console.log("1 * 3 = 3");
+console.log("1 * 4 = 4");
+console.log("1 * 5 = 5");
+console.log("1 * 6 = 6");
+console.log("1 * 7 = 7");
+console.log("1 * 8 = 8");
+console.log("1 * 9 = 9");
+console.log("1 * 10 = 10");
