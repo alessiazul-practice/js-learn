3df1f62feeca6972b5610d49c21565dfcf3724a0
alessiazul <alessiazul@gmail.com>
2025-08-28 22:01:26 -0500

    program multiplication

---
+++ 01-basics/003-mult-prog.js
@@ -0,0 +1,8 @@
+for (let v = 1; v <= 100000; v++) {
+    // 'v <= 3'
+    // '1 <= 3' True
+    // '2 <= 3' True
+    // '3 <= 3' True
+    // '4 <= 3' False
+    console.log("La Tabla del ", v);
+}
