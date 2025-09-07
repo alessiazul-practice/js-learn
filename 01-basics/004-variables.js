8bb68f4fe3658ad3b1140aaaa39bb164768d8558
alessiazul <alessiazul@gmail.com>
2025-09-07 16:53:53 -0500

    variables concatenation

---
+++ 01-basics/004-variables.js
@@ -0,0 +1,14 @@
+let a = "que contenga el texto poto";
+let b = "y que tenga el texto roto";
+console.log( a + b) 
+let c = "steal ";
+let d = "a ";
+let e = "brainrot";
+console.log( c + d + e )
+let f = "steal";
+let g = "a";
+let h = "brainrot";
+console.log(f + " " + g + " " + h);
+console.log(f, g, h);
+let i = " no te amo jsjs"
+console.log( i )
